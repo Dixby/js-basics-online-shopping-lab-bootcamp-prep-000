d4507ebddc573f9b5ec3014e640a9e98c0944035
Andrew Walker <hovercar00@gmail.com>
2017-12-23 03:27:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,16 +43,23 @@ function total() {
 }
 
 function removeFromCart(item) {
-  for (let i=0; i<cart.length; i++) {
+  let itemInCart = false;
+
+  for (let i = 0, l = cart.length; i < l; i++) {
     if (cart[i].hasOwnProperty(item)) {
-      cart.splice(i, 1);
-      return cart;
-  } else {
-    console.log("That item is not in your cart.");
-    return cart;
+      itemInCart = true;
+      cart = cart.slice(0, i).concat(cart.slice(i + 1));
+      l--;
     }
   }
+
+  if (!itemInCart) {
+    console.log("That item is not in your cart.");
+  }
+
+  return cart;
 }
+
 
 function placeOrder(cardNumber) {
   if (cardNumber === undefined)
