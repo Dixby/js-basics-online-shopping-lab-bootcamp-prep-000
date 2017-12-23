fdefac2250020bf02e8b300147f9f8c547e7fa34
Andrew Walker <hovercar00@gmail.com>
2017-12-23 02:51:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function addToCart(item) {
 
 function viewCart() {
   if (cart.length === 0) {
-    console.log("Your cart is empty.");
+    console.log("Your shopping cart is empty.");
   } else if (cart.length === 1) {
     console.log(`In your cart, you have ${Object.keys(cart[0])[0]} at $${Object.keys(cart[0])[0]}`);
   } else if (cart.length === 2) {
