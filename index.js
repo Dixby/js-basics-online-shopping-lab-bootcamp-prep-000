5326aaa94f680300174410297eea0ce7e13d04a9
Andrew Walker <hovercar00@gmail.com>
2017-12-23 03:07:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,6 +57,6 @@ function placeOrder(cardNumber) {
   if (cardNumber !== true) {
     console.log("Sorry, we don't have a have a credit card on file for you.");
   } else {
-    console.log(`Your total cost is $${total}`)
+    console.log(`Your total cost is $${total}, which will be charged to the card ${cardNumber}.`)
   }
 }
