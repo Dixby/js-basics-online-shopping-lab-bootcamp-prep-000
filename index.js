a2f569b9df9b98bb82eefb72866d024eec79247b
Andrew Walker <hovercar00@gmail.com>
2017-12-23 02:59:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,7 +45,8 @@ function total() {
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
     if (cart[i].hasOwnProperty(item) === true) {
-      
+      delete cart[i];
+      return cart
     }
   }
 }
