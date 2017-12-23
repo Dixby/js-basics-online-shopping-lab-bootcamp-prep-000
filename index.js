45fe68ace8c0fd0b4a49c037ee4691b9d881c0b6
Andrew Walker <hovercar00@gmail.com>
2017-12-23 02:24:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function viewCart() {
   else {
     var cartItems = [];
     for (var i = 0; i < cart.length; i++);
-    let item = Object.keys(cart[i])(0)
+    let item = Object.keys(cart[i])[0]
   }
   
 }
