9681e9ef054d26b5dbb52afa38692703a0a8a7c1
Andrew Walker <hovercar00@gmail.com>
2017-12-23 03:11:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function total() {
 
 function removeFromCart(item) {
   for (let i = 0; i < cart.length; i++) {
-  if (cart[i].hasOwnProperty(item) === true){
+  if (cart[i].hasOwnProperty(item)) {
     cart.splice(i, 1);
   } else {
       console.log("That item is not in your cart.");
@@ -58,6 +58,5 @@ function placeOrder(cardNumber) {
     console.log("Sorry, we don't have a have a credit card on file for you.");
   } else {
     console.log(`Your total cost is $${total}, which will be charged to the card ${cardNumber}.`);
-    return cart = [];
   }
 }
