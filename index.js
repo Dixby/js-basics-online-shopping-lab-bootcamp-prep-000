69ff114241f2a1862de1837ec50b8602cdbb9842
Andrew Walker <hovercar00@gmail.com>
2017-12-23 02:22:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function viewCart() {
     console.log("Your shopping cart is empty.");
   } else {
     var cartItems = [];
-    for (var i = 0; )
+    for (var i = 0; i < cart.length;)
   }
   
 }
