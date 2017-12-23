b9b87c42a64118e59c976a7c3d455b1232c22c65
Andrew Walker <hovercar00@gmail.com>
2017-12-23 03:25:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,9 +47,10 @@ function removeFromCart(item) {
     if (cart[i].hasOwnProperty(item)) {
       cart.splice(i, 1);
       return cart;
-  } 
-  console.log("That item is not in your cart.");
-  return cart;
+  } else {
+    console.log("That item is not in your cart.");
+    return cart;
+    }
   }
 }
 
