c8cd776a0b23e24b171bdb84ccbe89fc2a8947d8
Andrew Walker <hovercar00@gmail.com>
2017-12-23 02:49:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function viewCart() {
   } else if (cart.length === 1) {
     console.log(`In your cart, you have ${Object.keys(cart[0])[0]} at $${Object.keys(cart[0])[0]}`);
   } else if (cart.length === 2) {
-    
+    console.log(`In your cart, you have ${Object.keys(cart[0])[0]} at $${cart[0][Object.keys(cart[0])[0]]} and ${Object.keys(cart[1])[0]} at $${cart[1][Object.keys(cart[1])[0]]}.`);
   }
 }
 
